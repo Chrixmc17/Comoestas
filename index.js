b198bea8b1e0968d19a81b8983800c5a30949d46
Chrixmc17 <115506328+Chrixmc17@users.noreply.github.com>
2022-10-10 17:31:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yestBtn = document.querySelector('#yesBtn');
+
+yestBtn.addEventListener('click',function () {
+    alert('¡Me alegra que hayas tenido un buen dia!. ¡Sigue asi que poco a poco lograras lo que deseas!:3')
+});
+const noBtn = document.querySelector('#noBtn');
+noBtn.addEventListener('click',function () {
+    alert('Tuviste un dia agotador, eso significa que lo diste todo por hoy y te mereces un buen descanzo:D. Abrigate que hace frio y come bien. -w-')
+})
